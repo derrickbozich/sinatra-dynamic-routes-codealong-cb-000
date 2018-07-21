35bab64a975c3a7dea4472e0c8719ee7f65850fd
derrick b <dbozich1@gmail.com>
2018-07-21 02:33:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,12 +20,13 @@ class App < Sinatra::Base
   end
 
   get 'multiply/:num1/:num2' do
-    @num1 = params[:num1].to_i
-    @num2 = params[:num2].to_i
-
-    @product = @num1 * @num2
-
-    "#{@product}"
+    # @num1 = params[:num1].to_i
+    # @num2 = params[:num2].to_i
+    #
+    # @product = @num1 * @num2
+    #
+    # "#{@product}"
+    'hello world'
   end
 
 end
